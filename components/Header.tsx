4be505be1fa0227f47815070711a950e1b71c21a
Gleepy <107324298+mtwence@users.noreply.github.com>
2023-03-04 19:17:26 -0500

    get tailwind css working, section layout, and create header component

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function Header({}: Props) {
+  return (
+    <header>This be the Header</header>
+  )
+}
+
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Header from '@/components/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,22 @@ export default function Home() {
       <Head>
         <title>Cyber Mike</title>
       </Head>
-      
-    </>
+      <h1 className="p-10 text-red-500">Hello is this thing on???</h1>
+       
+      {/* Header */}
+        <Header />
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experiences */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact */}
+    </> 
   )
 }
