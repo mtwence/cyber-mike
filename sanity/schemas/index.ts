07ed72ebf115890e2e81c5c1c6a35282be0038c1
michael wence <mtwence@gmail.com>
2025-04-07 20:57:49 -0400

    sanity fix

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,10 @@
+import experience from "./experience";
+import pageDetails from "./pageDetails";
+import projects from "./projects";
+import skills from "./skills";
+import social from "./social";
+import education from "./education";
+
+const schemaTypes = [skills, pageDetails, social, experience, projects, education];
+
+export default schemaTypes; 
